7e383bc69f48d16cd396b5d91239e62bf84c7b0e
Artem Lodygin <art.lodigin@gmail.com>
2021-08-31 00:23:42 +0200

    fix daemonization

--- src/daemon.hh
+++ src/daemon.hh
@@ -1,17 +1,43 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <signal.h>
 
 #include <cstdio>
 #pragma once
 
+void fork_off() {
+    const auto pid = fork();
+    if (-1 == pid) {
+        perror("fork error");
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+}
+
 void daemonize(const char* dir) {
+    fork_off();
+
+    if (-1 == setsid()) { // does it require sudo? probably not
+        perror("setsid error");
+    }
+
+    // not sure if really necessary right now:
+    {
+        signal(SIGCHLD, SIG_IGN);
+        signal(SIGHUP, SIG_IGN);
+
+        fork_off();
+    }
+
+    // umask(...), but don't want to use it yet
+
     if (-1 == chdir(dir)) {
         perror("chdir error");
     }
-    if (-1 == setsid()) {
-        perror("setsid error");
-    }
+
     if (EOF == fclose(stdin)) {
         perror("fclose(stdin) error");
     }
@@ -21,4 +47,11 @@ void daemonize(const char* dir) {
     if (EOF == fclose(stderr)) {
         perror("fclose(stderr) error");
     }
+
+    // seems unnecessary right now
+    // for (int fd = sysconf(_SC_OPEN_MAX); 0 <= fd; --fd) {
+    //     if (-1 == close(fd)) {
+    //         perror("close error");
+    //     }
+    // }
 }
